ee719ce4ad95ee5ad18001f29291c00ef613aebc
wdy3110411073 <610488042@qq.com>
2023-07-19 10:21:46 +0800

    登录雪花效果的实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="login-container">
+    <div style="position: absolute;height: 100vh;width: 100%">
+      <div v-for="item in snow" class="snow"></div>
+
+    </div>
     <el-form ref="loginFromRef" class="login-form" :model="loginForm" :rules="loginRules">
       <div class="title-container">
         <h3 class="title">Vue3后台系统</h3>
@@ -64,6 +68,12 @@ import {getCode} from '@/api/api'
 import {useStore} from 'vuex'
 import {useRouter} from 'vue-router'
 import {ElMessage} from "element-plus";
+
+//雪花效果
+const snow = ref([])
+for(let i=0;i<1000;i++){
+  snow.value.push(i)
+}
 
 const code_net = ref('')
 
@@ -176,14 +186,14 @@ $light_gray: #eee;
 $cursor: #000000;
 $txt_color: #333;
 
-
 .login-container {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../../assets/login-background.jpeg");
-  background-size: cover;
+  //background-image: url("../../assets/login-background.jpeg");
+  //background-size: cover;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
   .login-form {
     position: relative;
     width: 420px;
@@ -278,4 +288,48 @@ $txt_color: #333;
     letter-spacing: 1px;
   }
 }
+
+@function random_range($min, $max) {
+  $rand: random();
+  $random_range: $min + floor($rand * (($max - $min) + 1));
+  @return $random_range;
+}
+
+.snow {
+  $total: 200;
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background: white;
+  border-radius: 50%;
+
+  @for $i from 1 through $total {
+    $random-x: random(1000000) * 0.0001vw;
+    $random-offset: random_range(-100000, 100000) * 0.0001vw;
+    $random-x-end: $random-x + $random-offset;
+    $random-x-end-yoyo: $random-x + ($random-offset / 2);
+    $random-yoyo-time: random_range(30000, 80000) / 100000;
+    $random-yoyo-y: $random-yoyo-time * 100vh;
+    $random-scale: random(10000) * 0.0001;
+    $fall-duration: random_range(10, 30) * 1s;
+    $fall-delay: random(30) * -1s;
+
+    &:nth-child(#{$i}) {
+      opacity: random(10000) * 0.0001;
+      transform: translate($random-x, -10px) scale($random-scale);
+      animation: fall-#{$i} $fall-duration $fall-delay linear infinite;
+    }
+
+    @keyframes fall-#{$i} {
+      #{percentage($random-yoyo-time)} {
+        transform: translate($random-x-end, $random-yoyo-y) scale($random-scale);
+      }
+
+      to {
+        transform: translate($random-x-end-yoyo, 100vh) scale($random-scale);
+      }
+    }
+  }
+}
+
 </style>
