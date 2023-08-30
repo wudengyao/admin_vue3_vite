6a48b138609e89501d1bc91ec3c3d9e8ab6a6f4f
wdy3110411073 <610488042@qq.com>
2023-08-30 11:06:08 +0800

    整理框架代码

--- src/main.js
+++ src/main.js
@@ -1,45 +1,39 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import installElementPlus from './plugins/element'
 import router from './router'
 import store from './store'
-
-
 const app = createApp(App)
+
 // 导入权限控制模块
 import './permission'
-
-import installDirective from '@/directives'
 import "@/styles/index.scss"
-import axios from '@/utils/axios'
 
-app.config.globalProperties.$axios = axios // 使用globalProperties挂载
+// import axios from '@/utils/axios'
+// app.config.globalProperties.$axios = axios // 使用globalProperties挂载
 
+//element
+import installElementPlus from './plugins/element'
 installElementPlus(app)
+//directives
+import installDirective from '@/directives'
 installDirective(app)
 // filter
 import installFilter from '@/filters'
-
 installFilter(app)
-
-
 
 // 自定义表格工具组件
 import RightToolbar from '@/components/RightToolbar'
 // 分页组件
 import Pagination from '@/components/Pagination'
-
+// svg组件
+import svgIcon from "@/components/SvgIcon/index.vue";
 // 全局组件挂载
 app.component('RightToolbar', RightToolbar)
 app.component('Pagination', Pagination)
+app.component('svg-icon', svgIcon)
 
-
-// 将自动注册所有组件为全局组件
-import svgIcon from "@/components/SvgIcon/index.vue";
-import 'virtual:svg-icons-register'  // 新增
 
 app
     .use(store)
     .use(router)
-    .component('svg-icon', svgIcon)
     .mount('#app')
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,6 +3,7 @@ import 'element-plus/dist/index.css'
 import locale from 'element-plus/lib/locale/lang/zh-cn';
 // 注册全部的svg图标
 import elementIcons from '@/plugins/svgicon'
+import 'virtual:svg-icons-register'
 
 export default app => {
   app
