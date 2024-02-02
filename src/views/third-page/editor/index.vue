fb7370ece6a6f14ea6a10aafb303d8f7771028d4
wudengyao <610488042@qq.com>
2024-02-02 15:18:16 +0800

    feat: 增加git提交规范

--- src/views/third-page/editor/index.vue
+++ src/views/third-page/editor/index.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="container">
-    <editor
-        :detail="detail"
-        @onSuccess="onSuccess"></editor>
-  </div>
+	<div class="container">
+		<editor
+			:detail="detail"
+			@onSuccess="onSuccess"
+		></editor>
+	</div>
 </template>
-
-<script setup>
-import Editor from '../components/Editor.vue'
-import { ref } from 'vue'
-const detail = ref({})
-
-detail.value ='富文本编辑器测试输入'
-// 创建成功
-const onSuccess = () => {
-  console.log("onSuccess")
-}
-
-</script>
 
 <style lang="scss" scoped>
 .container {
 }
 </style>
+
+<script setup>
+import Editor from "../components/Editor.vue";
+import { ref } from "vue";
+const detail = ref({});
+
+detail.value = "富文本编辑器测试输入";
+// 创建成功
+const onSuccess = () => {
+	console.log("onSuccess");
+};
+
+</script>
